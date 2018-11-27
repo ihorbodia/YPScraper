6d74c4e55a7259caad947164f97995342a1d0280
SIIPOLSKA\ibodia <ibodia@sii.pl>
2018-11-27 17:52:51 +0100

    Start refactoring.

--- YPScraper/src/ypscraper/YPScraper.java
+++ YPScraper/src/ypscraper/YPScraper.java
@@ -1,25 +1,30 @@
 package ypscraper;
 
-import gui.FrameMain;
-import java.awt.Dimension;
-import java.awt.EventQueue;
+import java.io.IOException;
+import javax.swing.text.Element;
+ import org.jsoup.Jsoup;
+ import org.jsoup.nodes.Document;
 
 public class YPScraper {
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    FrameMain frame = new FrameMain();
-                    frame.pack();
-                    Dimension frameSize = frame.getSize();
-                    frameSize.width += 100;
-                    frame.setMinimumSize(frameSize);
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    public static void main(String[] args) throws IOException {
+//        EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                try {
+//                    FrameMain frame = new FrameMain();
+//                    frame.pack();
+//                    Dimension frameSize = frame.getSize();
+//                    frameSize.width += 100;
+//                    frame.setMinimumSize(frameSize);
+//                    frame.setVisible(true);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
+        org.jsoup.nodes.Document doc = Jsoup.connect("https://www.yellowpages.ca/search/si/1/fashion%20retailer/ON").get();
+
+        Element link = (Element) doc.select("a").first();
     }
 }
